5c264c08a46ac3e260fc6a2689e0df17acb2e8bd
Mrudu03 <158609576+Mrudu03@users.noreply.github.com>
2024-02-23 21:17:27 +0000

    Added all changes

--- 07_Collection/comprehenssions_ex.py
+++ 07_Collection/comprehenssions_ex.py
@@ -139,8 +139,7 @@ original_text = "Attack is planned to happen on next Sunday"
 original_text = original_text.lower()
 print(f"Original text: {original_text}")
 shift_value = 3
-encrypted_text = [chr((ord(char) - ord('a') + shift_value) % 26 + ord('a')) if char.isalpha() else char for char in original_text]
+encrypted_text = [chr((ord(char) - ord('a') + shift_value) % 26  + ord('a')) if char.isalpha() else char for char in original_text]
 encrypted_text = "".join(encrypted_text)
-
 print(f"Original text: {original_text}")
 print(f"Encrypted text: {encrypted_text}")
